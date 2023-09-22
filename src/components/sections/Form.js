736ce3802c14ca5314671bd904e9ec315d26d99d
Taiwo Adepoju <tadepoju95@gmail.com>
2023-09-22 15:05:12 -0500

    Modified files in project folder.

--- src/components/sections/Form.js
+++ src/components/sections/Form.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 class Form extends React.Component {
 	state = { inputValue: "", errorMessage: "", color: "", translate: "", validateMd: "",
-			formPadding: "", placeholder: ""};
+			formPadding: "", placeholder: "", errorValidation: "", formButton: ""};
 
 	onInput = e => {
 		e.preventDefault();
@@ -30,22 +30,24 @@ class Form extends React.Component {
 		} else {
 			this.setState({ errorMessage: "please add a link"});
 			this.setState({ color: "hsl(0, 87%, 67%)"});
-			this.setState({ translate: "translateY(-1.9em)"});
+			this.setState({ translate: "translateY(46px)"});
 			this.setState({ validateMd: "validateMd"});
 			this.setState({ formPadding: "formPadding"});
 			this.setState({ placeholder: "color"});
+			this.setState({ errorValidation: "errorValidation"});
+			this.setState({ formButton: "formButton"});
 		}
 		this.setState({ inputValue: ""});
 	};
 
 	renderForm() {
 		return (
-				<div id="input-group" className={"mb-3 d-flex flex-column flex-md-row " + this.state.validateMd}>
+				<div id="input-group" className={"mb-3 " + this.state.validateMd}>
 		  			<input type="text" className={"form-control " + this.state.placeholder} placeholder="Shorten a link here..." 
 		  			aria-label="Recipient's username" aria-describedby="button-addon2" 
-		  			value={this.state.inputValue} style={{borderColor: this.state.color, transform: this.state.translate}}
+		  			value={this.state.inputValue} style={{borderColor: this.state.color}}
 		  			onChange={(e) => this.setState({inputValue: e.target.value}, () => {console.log(this.state.inputValue)})} />
-	    			<button className="btn" type="submit" id="button-addon2">Shorten It!</button>
+					<p className={"validation-text " + this.state.errorValidation}>{this.state.errorMessage}</p>
 	  			</div>
 
 		)
@@ -54,11 +56,9 @@ class Form extends React.Component {
 	
 	render() {
 		return (
-			<form className={"form " + this.state.formPadding} onInput={this.onInput} onSubmit={this.onSubmit}>
-				<div className="form-input" style={{backgroundImage: 'url("/images/bg-shorten-mobile.svg")', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
-					<p className="validation-text">{this.state.errorMessage}</p>
-					{this.renderForm()}
-				</div>
+			<form className={"form d-flex flex-column flex-md-row " + this.state.formPadding} onInput={this.onInput} onSubmit={this.onSubmit}>	
+				{this.renderForm()}
+				<button className={"btn " + this.state.formButton} type="submit" id="button-addon2">Shorten It!</button>
 			</form>
 		)
 	}
